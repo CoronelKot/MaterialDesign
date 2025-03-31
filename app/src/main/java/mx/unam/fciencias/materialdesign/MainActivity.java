ec7d03130aae108c3427a17b0eba298efcc5f3aa
Pablocro <pablocro@ciencias.unam.mx>
2025-03-30 21:13:44 -0600

    ListaInfinita implementada, junto con la Navegacion.

--- app/src/main/java/mx/unam/fciencias/materialdesign/MainActivity.java
+++ app/src/main/java/mx/unam/fciencias/materialdesign/MainActivity.java
@@ -1,6 +1,9 @@
 package mx.unam.fciencias.materialdesign;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.launch_second_activity);
+        button.setOnClickListener(this::launchSecondActivity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void launchSecondActivity(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
     }
 }
